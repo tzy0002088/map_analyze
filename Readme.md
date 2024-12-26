a4f6353c1d11a2f6c54d53e124b619e86819608a
tangzhenye <zhenye.tang@timesintelli.com>
2024-12-26 15:32:19 +0800

    【update】Improve code

--- Readme.md
+++ Readme.md
@@ -1,20 +1,38 @@
-# map analyze
+## map_analyze
 
-## 介绍
+### 1. 介绍
 
-这是一个用于统计固件中各模块资源占用的工具，使用命令如下：
+​	map_analyze 用于分析 elf map 文件，能够方便分析出 `指定模块` 在可执行文件中各的静态内存占用。
 
-```
-python3 map_analyze.py -m xxxx.map -k usb_descriptors
-```
+### 2. 如何使用
 
-这样子会将 usb_descriptors.o 所占用的资源统计出来，输出如下：
+#### 2.1 命令行中输入：
 
 ```
-.text: 170 bytes
-.rodata: 365 bytes
-.data.string_desc_arr: 20 bytes
-.data: 2 bytes
-.bss: 64 bytes
+python3 map_analyze.py -m at820_alg_standard_demo.map -k rtos
 ```
 
+命令行参数：
+
+* -m: map 文件路径
+* -k: 关键字，即模块名
+
+> 模块的最小单位为一个 C 文件
+
+#### 2.2 输出：
+
+```
+.sdata: 8 bytes
+.sbss: 124 bytes
+.bss: 141724 bytes
+.text: 12472 bytes
+.rodata: 157 bytes
+------------------------------
+L2SRAM: 141856 bytes
+FLASH: 12629 bytes
+```
+
+输出说明：
+
+* 上面一部分为 rtos 模块（rtos 目录下的所有 C 文件）各 section 占用
+* 下面一部分为各 section 在最终 内存/flash 中的占用，如 FLASH 的 12629 bytes 包含 .text 段与.rodata 段，其余 section 全部在 L2SRAM 中
--- map_analyze.py
+++ map_analyze.py
@@ -3,64 +3,79 @@ import os
 import argparse
 from collections import defaultdict
 
-whitelist = ['.itcm_code', '.bss', '.sbss', '.data', '.sdata', '.dtcm_data', '.dtcm_data_no_load', '.text', '.flash_rodata', '.rodata']
+class MapAnalyzer:
+    def __init__(self, map_file: str, keyword: str, section_whitelist=None, mem_layout=None):
+        self.__map_file = map_file
+        self.__keyword = keyword
+        self.__whitelist = section_whitelist or [".itcm_code", ".bss", ".sbss", ".data", ".sdata", ".dtcm_data", ".dtcm_data_no_load", ".text", ".flash_rodata", ".rodata"]
+        self.__mem_layout = mem_layout or {
+            "L2SRAM": [0x10000000, 0x10030000],
+            "L1_ITCM": [0, 0x8000],
+            "L1_DTCM": [0x00010000, 0x00018000],
+            "L1CC": [0x30000000, 0x30010000],
+            "FLASH": [0x10100000, 0x10300000],
+        }
 
-def preprocess_map_file(map_path: str) -> str:
-    """Preprocess the map file to retain only content after 'Memory Configuration'."""
-    with open(map_path, "r") as infile:
-        lines = infile.readlines()
-    start_index = next((i for i, line in enumerate(lines) if "Memory Configuration" in line), None)
+    def __preprocess_map_file(self) -> str:
+        with open(self.__map_file, "r") as infile:
+            lines = infile.readlines()
+        start_index = next((i for i, line in enumerate(lines) if "Memory Configuration" in line), None)
+        if start_index is None:
+            raise ValueError("'Memory Configuration' not found in the map file.")
 
-    if start_index is None:
-        raise ValueError("'Memory Configuration' not found in the map file.")
+        temp_file = self.__map_file + ".processed"
+        with open(temp_file, "w") as outfile:
+            outfile.writelines(lines[start_index:])
+        return temp_file
 
-    temp_file = map_path + ".processed"
-    with open(temp_file, "w") as outfile:
-        outfile.writelines(lines[start_index:])
+    def __get_whitelisted_name(self, section_name):
+        exact_matches = {whitelisted: whitelisted for whitelisted in self.__whitelist}
+        partial_matches = [whitelisted for whitelisted in self.__whitelist if whitelisted in section_name]
+        return exact_matches.get(section_name) or (partial_matches[0] if partial_matches else None)
 
-    return temp_file
+    def __analyze(self):
+        section_usage = defaultdict(int)
+        mem_usage = defaultdict(int)
+        processed_map = self.__preprocess_map_file()
 
-def map_analyze(map: str, keyword: str):
-    section_usage = defaultdict(int)
-    same_line_pattern = re.compile(
-        r'^\s*(\.\S+|COMMON)\s+0x[0-9a-fA-F]+\s+0x([0-9a-fA-F]+)\s+.*?\b' + re.escape(keyword) + r'\S*\.o\b'
-    )
-    size_pattern = re.compile(r'\s+0x[0-9a-fA-F]+\s+0x([0-9a-fA-F]+)')
-    section_name_pattern = re.compile(r'^\s*(\.\S+)')
+        pattern = re.compile(
+            r"^\s*(\.\S+|COMMON)"
+            r"(?:\s+0x[0-9a-fA-F]+\s+0x([0-9a-fA-F]+)\s+.*?\b" + re.escape(self.__keyword) + r"\S*\.o\b"
+            r"|(?:\s*\n\s*0x[0-9a-fA-F]+\s+0x([0-9a-fA-F]+)\s+.*?\b" + re.escape(self.__keyword) + r"\S*\.o\b))",
+            re.MULTILINE
+        )
+        address_pattern = r"\s(0x[0-9a-fA-F]+)\s+0x"
 
-    def get_whitelisted_name(section_name):
-        return next((whitelisted for whitelisted in whitelist if section_name.startswith(whitelisted)), None)
-
-    with open(map, "r") as f:
-        previous_line = ""
-        for current_line in f:
-            current_line = current_line.strip()
-
-            match = same_line_pattern.search(current_line)
-            if match:
-                section_name = match.group(1)  # .text.HAL_I2S_Init
-                size = int(match.group(2), 16)
-                if (whitelisted_name := get_whitelisted_name(section_name)):
+        with open(processed_map, "r") as f:
+            content = f.read()
+            matches = pattern.finditer(content)
+            for match in matches:
+                section_name = match.group(1)
+                size = match.group(2) or match.group(3)
+                size = int(size, 16)
+                symble_addr = re.findall(address_pattern, match.group(0))
+                if (whitelisted_name := self.__get_whitelisted_name(section_name)):
                     section_usage[whitelisted_name] += size
-                continue
+                    for region_name, address_range in self.__mem_layout.items():
+                        start_addr, end_addr = address_range
+                        if start_addr <= int(symble_addr[0], 16) < end_addr:
+                            mem_usage[region_name] += size
+        os.remove(processed_map)
 
-            if re.search(r'\b' + re.escape(keyword) + r'\S*\.o\b', current_line):
-                section_match = section_name_pattern.search(previous_line)
-                size_match = size_pattern.search(previous_line + " " + current_line)
-                if section_match and size_match:
-                    section_name = section_match.group(1)  # .rodata.HAL_I2S_Config
-                    size = int(size_match.group(1), 16)
-                    if (whitelisted_name := get_whitelisted_name(section_name)):
-                        section_usage[whitelisted_name] += size
+        return section_usage, mem_usage
 
-            previous_line = current_line
+    def section_usage(self):
+        section_usage, _ = self.__analyze()
+        return section_usage
 
-    return section_usage
+    def mem_usage(self):
+        _, mem_usage = self.__analyze()
+        return mem_usage
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Map analyze tool')
     parser.add_argument('-m', '--map', type=str, help='Map file path', required=True)
-    parser.add_argument('-k', '--keyword', type=str, help='Module keyword, e.g., [middleware]', required=True)
+    parser.add_argument('-k', '--keyword', type=str, help='Module keyword, e.g., [rtos]', required=True)
 
     args = parser.parse_args()
     map = args.map
@@ -68,11 +83,14 @@ if __name__ == "__main__":
 
     try:
         if os.path.isfile(map):
-            processed_map = preprocess_map_file(map)
-            section_usage = map_analyze(processed_map, keyword)
-            os.remove(processed_map)
+            analyzer = MapAnalyzer(map, keyword)
+            section_usage = analyzer.section_usage()
+            mem_usage = analyzer.mem_usage()
             for section, size in section_usage.items():
                 print(f"{section}: {size} bytes")
+            print('------------------------------')
+            for region, size in mem_usage.items():
+                print(f"{region}: {size} bytes")
         else:
             print(f"Error: File '{map}' does not exist.")
     except Exception as e:
